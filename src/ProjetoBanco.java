1d8c432b6c0c621583932cc41adc6ea08ed5dfcb
João Victor Quintino de Brito <joaovictorquintinobrito@gmail.com>
2025-03-28 16:03:58 -0300

    Alterações nas funcionalidades

--- src/ProjetoBanco.java
+++ src/ProjetoBanco.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class ProjetoBanco {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double valorTransferido;
-        double valoRecebido;
-        double saldo = 2500.00;
-        int opcao = 0;
+        double sacar;
+        double depositar;
+        var saldo = 2500.00;
+        var opcao = 0;
         System.out.println("""
                 ********************************************
-                Dados iniais do cliente:
+                Dados iniciais do cliente:
                 
-                Nome:  João Victor Quintino de Brito
-                Tipo conta: Corrente
-                Saldo inicial: R$ 2500.00
+                Nome: João Victor Quintino de Brito
+                Tipo da conta: Corrente
+                Saldo atual: R$ 2500.00
                 ********************************************""");
         while (opcao != 4 ){
             System.out.println("""
@@ -21,31 +21,31 @@ public class ProjetoBanco {
                     Operações
                     
                     1 - Consultar saldo
-                    2 - Receber valor
-                    3 - Transferir valor
+                    2 - Depositar
+                    3 - Sacar
                     4 - Sair
                     
                     Digite a Opção desejada:
                     """);
             opcao = scanner.nextInt();
             if (opcao == 1){
-                System.out.println("O saldo da sua conta é: "+saldo);
+                System.out.println("O saldo atual é de: "+saldo);
             } else if (opcao == 2) {
-                System.out.println("Informe o valor a receber");
-                valoRecebido = scanner.nextDouble();
-                saldo += valoRecebido;
-                System.out.println("Saldo atualizado: " +saldo);
+                System.out.println("Informe o valor a depositar: ");
+                depositar = scanner.nextDouble();
+                saldo += depositar;
+                System.out.println("Saldo depositado com sucesso!");
+                System.out.println("Seu saldo agora é de: "+saldo);
             } else if (opcao == 3) {
-                System.out.println("Infome o valor que deseja transferir");
-                valorTransferido = scanner.nextDouble();
-                if (valorTransferido < saldo) {
-                    saldo = saldo - valorTransferido;
-                    System.out.println("Seu saldo atual agora é de: " + saldo);
+                System.out.println("Informe o valor que deseja sacar: ");
+                sacar = scanner.nextDouble();
+                if (sacar < saldo) {
+                    saldo -= sacar;
+                    System.out.println("Seu saldo atual é de: "+saldo);
                 } else {
-                    System.out.println("Não há saldo sufiente para fazer essa transferência.");
+                    System.out.println("Não há saldo suficiente para fazer esse saque.");
                 }
-            }
-            if (opcao > 4 ) {
+            }if (opcao > 4 ) {
                 System.out.println("Opcao invalida!");
             }
         }
